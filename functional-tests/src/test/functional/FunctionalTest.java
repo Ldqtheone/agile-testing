fb14464dcd1bf0531aae667b8d7240e12494bb98
Dreaxxx <ldqtheone@gmail.com>
2021-01-11 16:26:50 +0100

    mid story 2 research done

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -5,11 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FunctionalTest {
 
@@ -27,18 +29,36 @@ public class FunctionalTest {
     // Test de la Story #1-homepage (https://trello.com/c/WKTneu9o/1-homepage)
 	@Test
     public void testHomepage() throws Exception {
+
+        String h1 = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[1]/h1/span";
+        String subH1 = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[2]/p/span";
+        String joinButton = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[3]/a";
+
         driver.get("https://www.meetup.com/fr-FR/");
 		assertEquals(driver.getTitle(), "Partagez vos passions | Meetup");
-		assertEquals(driver.findElement(By.className("exploreHome-hero-mainTitle")).getText(), "Le monde vous tend les bras");
-		assertEquals(driver.findElement(By.className("exploreHome-hero-subTitle")).getText(), "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
-		assertEquals(driver.findElement(By.id("joinMeetupButton")).getCssValue("background"), "rgb(241, 58, 89) none repeat scroll 0% 0% / auto padding-box border-box");
-		assertEquals(driver.findElement(By.id("joinMeetupButton")).getText(), "Rejoindre Meetup");
-		assertEquals(driver.findElement(By.id("joinMeetupButton")).getAttribute("href"), "https://www.meetup.com/fr-FR/register/");
+		assertEquals(driver.findElement(By.xpath(h1)).getText(), "Le monde vous tend les bras");
+		assertEquals(driver.findElement(By.xpath(subH1)).getText(), "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
+		assertEquals(driver.findElement(By.xpath(joinButton)).getCssValue("background"), "rgb(241, 58, 89) none repeat scroll 0% 0% / auto padding-box border-box");
+		assertEquals(driver.findElement(By.xpath(joinButton)).getText(), "Rejoindre Meetup");
+		assertEquals(driver.findElement(By.xpath(joinButton)).getAttribute("href"), "https://www.meetup.com/fr-FR/register/");
     }
 
-    // Test de la Story n ...
-    // TODO
-    // To Be Completed By Coders From Coding Factory
+    @Test
+    public void testResearch() throws Exception {
+
+        String h1 = "/html/body/div[3]/div[1]/div[1]/h1";
+        String navBar = "/html/body/div[3]/div[1]/div[4]/div[1]";
+
+        driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
+
+        WebElement navbarComponent = driver.findElement(By.xpath(navBar));
+
+        assertTrue(driver.getTitle().contains("Nature et aventure"));
+        assertTrue(driver.findElement(By.xpath(h1)).getText().contains("Nature et aventure"));
+
+        assertTrue(navbarComponent.isEnabled());
+        assertTrue(navbarComponent.findElement(By.id("mainKeywords")).isEnabled());
+    }
 
     @After
     public void tearDown() throws Exception {
