37b3e39ec0f7b6e8845575d274480ced2f509d18
Dreaxxx <ldqtheone@gmail.com>
2021-01-11 15:34:46 +0100

    Functionnal test Homepage Story

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -3,6 +3,7 @@ package test.functional;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -28,8 +29,11 @@ public class FunctionalTest {
     public void testHomepage() throws Exception {
         driver.get("https://www.meetup.com/fr-FR/");
 		assertEquals(driver.getTitle(), "Partagez vos passions | Meetup");
-		// TODO
-		// To Be Completed By Coders From Coding Factory
+		assertEquals(driver.findElement(By.className("exploreHome-hero-mainTitle")).getText(), "Le monde vous tend les bras");
+		assertEquals(driver.findElement(By.className("exploreHome-hero-subTitle")).getText(), "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
+		assertEquals(driver.findElement(By.id("joinMeetupButton")).getCssValue("background"), "rgb(241, 58, 89) none repeat scroll 0% 0% / auto padding-box border-box");
+		assertEquals(driver.findElement(By.id("joinMeetupButton")).getText(), "Rejoindre Meetup");
+		assertEquals(driver.findElement(By.id("joinMeetupButton")).getAttribute("href"), "https://www.meetup.com/fr-FR/register/");
     }
 
     // Test de la Story n ...
