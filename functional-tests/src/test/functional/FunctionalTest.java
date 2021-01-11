588e1641a247a981785cf9b76b70dcc1c7ac6358
Dreaxxx <ldqtheone@gmail.com>
2021-01-11 15:10:55 +0100

    add functionnal test

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -1,18 +1,14 @@
 package test.functional;
 
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
-
-import static org.junit.Assert.*;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 public class FunctionalTest {
 
@@ -20,7 +16,7 @@ public class FunctionalTest {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver","/Library/Java/JUNIT/chromedriver");
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 		driver = new ChromeDriver();
 	    	// Seems no more working in last Chrome versions
 		// driver.manage().window().maximize();
